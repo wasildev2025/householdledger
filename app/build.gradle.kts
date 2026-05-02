c454494a35be590d05e33098564a4519e7dd97eb
wasildev2025 <wasildev2025@gmail.com>
2026-05-02 19:45:11 +0500

    upgrade JDK to version 21 and configure toolchain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,12 @@ plugins {
     alias(libs.plugins.ksp)
 }
 
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 android {
     namespace = "com.example.householdledger"
     compileSdk = 35
@@ -31,11 +37,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "21"
     }
     buildFeatures {
         compose = true
